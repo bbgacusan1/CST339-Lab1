6b71a475dfad38e9f9cff81bd5aaa3514a79cfc1
bbgacusan1 <bbgacusan1@gmail.com>
2026-09-17 22:22:17 -0700

    Add SLF4L logging to controller and service

--- lab1-api/src/main/java/com/gcu/lab1_api/service/HelloService.java
+++ lab1-api/src/main/java/com/gcu/lab1_api/service/HelloService.java
@@ -4,9 +4,15 @@ import org.springframework.stereotype.Service;
 
 import com.gcu.lab1_api.dto.HelloResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @Service
 public class HelloService {
+    private static final Logger logger = LoggerFactory.getLogger(HelloService.class);
+
     public HelloResponse getHello() {
+        logger.info("Generating hello message");
         return new HelloResponse("Hello, Spring Boot");
     }
 }
