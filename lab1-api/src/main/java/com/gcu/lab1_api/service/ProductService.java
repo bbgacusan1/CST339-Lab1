31f1319bd4b03900b0a53ee61980116b725fd3f2
bbgacusan1 <bbgacusan1@gmail.com>
2026-09-18 14:37:33 -0700

    Add CRUD methods to ProductService with unit tests

--- lab1-api/src/main/java/com/gcu/lab1_api/service/ProductService.java
+++ lab1-api/src/main/java/com/gcu/lab1_api/service/ProductService.java
@@ -18,4 +18,33 @@ public class ProductService {
     public List<Product> getAllProducts() {
         return productRepository.findAll();
     }
+
+    public Product getProductById(Long id) {
+        return productRepository.findById(id);
+    }
+
+    public Product createProduct(Product product) {
+        return productRepository.save(product);
+    }
+
+    public Product updateProduct(Long id, Product updatedProduct) {
+        Product existingProduct = productRepository.findById(id);
+        if (existingProduct != null) {
+            existingProduct.setName(updatedProduct.getName());
+            existingProduct.setPrice(updatedProduct.getPrice());
+            existingProduct.setDescription(updatedProduct.getDescription());
+            existingProduct.setQuantity(updatedProduct.getQuantity());
+            return productRepository.save(existingProduct);
+        }
+        return null;
+    }
+
+    public boolean deleteProduct(Long id) {
+        Product existingProduct = productRepository.findById(id);
+        if (existingProduct != null) {
+            productRepository.deleteById(id);
+            return true;
+        }
+        return false;
+    }
 }
